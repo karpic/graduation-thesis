d01224d735c0563c94b0726b76d8d46ae1f77ea6
Arsenije Karpic <arsenije.karpic@gmail.com>
2018-12-29 15:59:46 +0100

    Routes are now protected

--- src/main.js
+++ src/main.js
@@ -27,22 +27,21 @@ export const router = new VueRouter({
   routes
 });
 
-/* router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
-    if (!auth.loggedIn()) {
-      next({
-        path: '/signin',
-        query: { redirect: to.fullPath }
-      })
-    } else {
-      next()
-    }
-  } else {
-    next() // make sure to always call next()!
-  }
-}); */
+router.beforeEach((to, from, next) => { 
+  let signedIn = store.getters.isSignedIn;
+  if (to.matched.some(record => record.meta.requiresAuth)) { 
+      if (!signedIn) { 
+          next({ 
+              path: '/signin', 
+              query: { redirect: to.fullPath } 
+          }) 
+      } else { 
+          next();
+      } 
+  } else { 
+      next();
+  } 
+}) 
 
 new Vue({
   el: '#app',
