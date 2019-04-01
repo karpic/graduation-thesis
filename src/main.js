fb47b0ecaa09031a367b5f96dae1b24cad48ee0a
Arsenije Karpic <arsenije.karpic@gmail.com>
2019-04-01 20:55:15 +0200

    Init for mailer2

--- src/main.js
+++ src/main.js
@@ -1,36 +1,12 @@
-import store from './store/store';
-import VueAlertify from 'vue-alertify';
-import { mapMutations } from 'vuex';
-import { router } from './router';
-import App from './App.vue'
-import DateFilter from './filters/date';
 import Vue from 'vue'
-import VueGAPI from 'vue-gapi';
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.use(VueGAPI, apiConfig);
-Vue.use(VueAlertify);
-
-Vue.filter('date', DateFilter);
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  el: '#app',
-  render: h => h(App),
   store,
-  created() {
-    let signedIn;
-    this.$getGapiClient().then(gapi => {
-      signedIn = gapi.auth2.getAuthInstance().isSignedIn.get(); 
-      this.$store.commit('SET_ALERTIFY_INSTANCE', this.$alertify)
-      if(signedIn == true) {
-        console.log('Got into if');
-        this.$store.commit('SET_SIGNED_IN', true);
-        this.$getGapiClient().then(gapi=>{
-          this.$store.commit('SET_GAPI_INSTANCE', gapi);
-        });
-      } else {
-        this.$store.commit('SET_SIGNED_IN', false);
-      }
-    });
-  }
-})
+  render: h => h(App)
+}).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,23 +1,25 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router';
-import { routes } from './routes';
+import Router from 'vue-router'
+import Home from './views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(Router)
 
-export const router = new VueRouter({
-    mode: 'history',
-    routes
-  });
-  
-router.beforeEach((to, from, next) => { 
-    const publicPages = ['/signin'];
-    const authRequired = !publicPages.includes(to.path);
-    const loggedIn = localStorage.getItem('user');
-
-    if(authRequired && !loggedIn) {
-        next('/signin');
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
-
-    next();
-});
- 
+  ]
+})
